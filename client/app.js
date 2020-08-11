398f31a8ec5dc881c72fe49cec067182fe94e003
Maddie Higgins <maddiehiggins9@gmail.com>
2020-08-10 22:02:26 -0500

    first commit, basic setup and appropriate installations

--- client/app.js
+++ client/app.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import anime from 'animejs/lib/anime.es.js'
 
 import {Navbar} from './components'
 import Routes from './routes'
 
 const App = () => {
+  const animationRef = useRef(null)
+  React.useEffect(() => {
+    animationRef.current = anime({
+      targets: '.el',
+      translateX: 250,
+      delay: function(el, i) {
+        return i * 100
+      },
+      loop: true,
+      direction: 'alternate',
+      easing: 'easeInOutSine'
+    })
+  }, [])
+
   return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
+    <>
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+      <div className="App">
+        <button type="button" onClick={() => animationRef.current.restart()}>
+          Restart
+        </button>
+        <div className="el" />
+      </div>
+    </>
   )
 }
 
